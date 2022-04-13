29eca16cce00862665993f2321f62ea948266d9f
angelbonet <angel.bonet@gmail.com>
2022-04-13 10:17:38 +0200

    Create renew-cert_Mac.sh

---
+++ renew-cert_Mac.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# MODIFIED VERSION FROM https://github.com/jon91/FileMaker-LetsEncrypt-CentOS-7
+# --- Adapted for Ubuntu
+
+# Enter the path to your FileMaker Server directory, ending in a slash 
+SERVER_PATH="/opt/FileMaker/FileMaker Server/"
+
+FMADMIN="fmConsoleLogin"
+FMPASS="fmConsolePassword"
+
+#
+# --- you shouldn't need to edit anything below this line
+#
+
+WEB_ROOT="${SERVER_PATH}HTTPServer/htdocs"
+
+
+# Get the certificate
+certbot renew
+
+cp "/etc/letsencrypt/live/${DOMAIN}/fullchain.pem" "${SERVER_PATH}CStore/fullchain.pem"
+cp "/etc/letsencrypt/live/${DOMAIN}/privkey.pem" "${SERVER_PATH}CStore/privkey.pem"
+
+chmod 640 "${SERVER_PATH}CStore/privkey.pem"
+
+# Move an old certificate, if there is one, to prevent an error
+# changed to server.pem as that seems to be used by fm now instead
+mv "${SERVER_PATH}CStore/server.pem" "${SERVER_PATH}CStore/serverKey-old.pem"
+
+# Remove the old certificate
+fmsadmin certificate delete --yes -u ${FMADMIN} -p ${FMPASS}
+
+# Install the certificate
+fmsadmin certificate import "${SERVER_PATH}CStore/fullchain.pem" --keyfile "${SERVER_PATH}CStore/privkey.pem" -y -u ${FMADMIN} -p ${FMPASS}
+
+# Restart FM service MacOS
+sudo launchctl kickstart -k com.filemaker.fms
